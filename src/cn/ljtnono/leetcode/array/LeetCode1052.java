50c1e12c70bf430b05e003b2d8b11eeacd7716e5
lingjiatong <935188400@qq.com>
2022-11-09 01:18:43 +0800

    [feat]完成1423题

--- src/cn/ljtnono/leetcode/array/LeetCode1052.java
+++ src/cn/ljtnono/leetcode/array/LeetCode1052.java
@@ -11,13 +11,13 @@ public class LeetCode1052 {
 
     /**
      * 有一个书店老板，他的书店开了n分钟。每分钟都有一些顾客进入这家商店。给定一个长度为 n 的整数数组 customers ，其中 customers[i] 是在第 i 分钟开始时进入商店的顾客数量，所有这些顾客在第 i 分钟结束后离开。
-     *
+     * <p>
      * 在某些时候，书店老板会生气。 如果书店老板在第 i 分钟生气，那么 grumpy[i] = 1，否则 grumpy[i] = 0。
-     *
+     * <p>
      * 当书店老板生气时，那一分钟的顾客就会不满意，若老板不生气则顾客是满意的。
-     *
+     * <p>
      * 书店老板知道一个秘密技巧，能抑制自己的情绪，可以让自己连续minutes分钟不生气，但却只能使用一次。
-     *
+     * <p>
      * 请你返回 这一天营业下来，最多有多少客户能够感到满意 。
      *
      * <p>
@@ -26,41 +26,43 @@ public class LeetCode1052 {
      * 解释：书店老板在最后 3 分钟保持冷静。
      * 感到满意的最大客户数量 = 1 + 1 + 1 + 1 + 7 + 5 = 16.
      * </p>
+     *
      * @param customers
      * @param grumpy
      * @param minutes
      * @return
      */
     public static int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-        // 思路：求customers数组中长度为minutes的子数组的区间下标
-        int sum = 0, len = customers.length;
+        // 思路：首先 找到不改变的时候客人就满意的数量和 同时更新数组
+        // 这样问题就转变为 求数组指定长度最大和的问题
+        int len = grumpy.length;
+        int sum = 0;
         for (int i = 0; i < len; i++) {
-            if (grumpy[i] == 0){
+            if (grumpy[i] == 0) {
                 sum += customers[i];
                 customers[i] = 0;
             }
         }
-        int num = customers[0];
-        int maxval = customers[0];
-        for (int i = 1; i < len; i++){
-            if (i < minutes) num = num + customers[i];
-            else num = num + customers[i] - customers[i - minutes];
-            maxval = Math.max(maxval, num);
+
+        // 滑动窗口求customers数组中指定minutes长度最大子数组和
+        int maxSum = 0;
+        int currentMaxSum = 0;
+        for (int i = 0; i < len; i++) {
+            // 当小于minutes时
+            if (i < minutes) {
+                currentMaxSum += customers[i];
+            } else {
+                currentMaxSum += customers[i] - customers[i - minutes];
+            }
+            maxSum = Math.max(maxSum, currentMaxSum);
         }
 
-        return (sum + maxval);
-    }
+        return sum + maxSum;
 
-    public static int getSum(int[] arr, int startIndex, int endIndex) {
-        int result = 0;
-        for (int i = startIndex; i <= endIndex; i++) {
-            result += arr[i];
-        }
-        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(maxSatisfied(new int[] {2, 6, 6, 9}, new int[] {0, 0, 1, 1}, 1));
+        System.out.println(maxSatisfied(new int[]{2, 6, 6, 9}, new int[]{0, 0, 1, 1}, 1));
     }
 
 }
